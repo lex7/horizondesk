b2683e9064fd4238dd7082287e5dcb8781b3287d
timofmax <timofmax@gmail.com>
2024-08-06 00:46:35 +0300

    Master adjustment

---
+++ iOSApp/Horizon/Utilities/Models/Comon Models/HorizonModels/MasterApproveModel.swift
@@ -0,0 +1,25 @@
+//
+//  MasterDesiction.swift
+//  DeskHorizon
+//
+//  Created by Timofey Privalov on 06.08.2024.
+//
+
+import Foundation
+//case masterApprove(model MasterApproveModel)
+//case masterDeny(model MasterDenyModel)
+
+/*
+ {
+   "user_id": 0,
+   "request_id": 0,
+   "deadline": "string"
+ }
+ */
+struct MasterApproveModel {
+    
+}
+
+case MasterDenyModel {
+    
+}
---
+++ iOSApp/Horizon/Utilities/Models/Comon Models/HorizonModels/MasterDenyModel.swift
@@ -0,0 +1,8 @@
+//
+//  MasterDenyModel.swift
+//  DeskHorizon
+//
+//  Created by Timofey Privalov on 06.08.2024.
+//
+
+import Foundation
