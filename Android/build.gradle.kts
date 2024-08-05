2ea923be8a2d454e67075f1ab42f994940087e13
Дмитрий <82208286+st0rmtr0oper@users.noreply.github.com>
2024-08-05 23:28:55 +0700

    Add files via upload

---
+++ Android/build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.5" apply false
+    id ("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+}
