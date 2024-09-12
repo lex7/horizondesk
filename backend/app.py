32e0d90fcd3fc05dd93706f27920535a4829dcbb
Lex7 <alexray9228@gmail.com>
2024-09-12 22:03:40 +0500

    fix login

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 300
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 app = FastAPI()
 
